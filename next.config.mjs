263a030bf2457c3629f153ce164cf3af63b9518a
Carlos Ignacio Azócar Huerta <carlos.iah17@gmail.com>
2024-03-17 13:06:46 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/download',
-          destination: 'https://rr4---sn-uxgg5-njal7.googlevideo.com/videoplayback',
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
